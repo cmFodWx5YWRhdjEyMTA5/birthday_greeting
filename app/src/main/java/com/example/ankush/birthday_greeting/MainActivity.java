eaa39e052fc89700dec7450bdf7083210e795b3d
rawkush <ankushrawat950@gmail.com>
2018-02-05 21:44:13 +0530

    changing text to greet

--- app/src/main/java/com/example/ankush/birthday_greeting/MainActivity.java
+++ app/src/main/java/com/example/ankush/birthday_greeting/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ankush.birthday_greeting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Greet(View view)
+    {
+        TextView message= (TextView) findViewById(R.id.message);
+        EditText name = (EditText) findViewById(R.id.name);
+        String newName = name.getText().toString();
+        message.setText("Happy Birthday "+newName);
+
+    }
+
 }
